a9e99dbe3676d5227919995a8480dc48f74609f7
Sander Jyhne <sandsjyhne@gmail.com>
2022-01-12 01:12:06 +0100

    Added boundary mask metric

--- models/metrics/metrics.py
+++ models/metrics/metrics.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
+# GitHub repo: https://github.com/bowenc0221/boundary-iou-api
 
+
+import tensorflow as tf
+import cv2
+import numpy as np
 
 def mean_iou(y_true, y_pred):
     y_true = tf.cast(y_true, tf.dtypes.float64)
@@ -14,5 +18,47 @@ def dice_coefficient(y_true, y_pred, smooth=1):
     i = tf.reduce_sum(y_true * y_pred, axis=[1, 2, 3])
     u = tf.reduce_sum(y_true, axis=[1, 2, 3]) + tf.reduce_sum(y_pred, axis=[1, 2, 3])
     dice = tf.reduce_mean((2. * i + smooth) / (u + smooth), axis=0)
-    
+
     return dice
+
+
+# General util function to get the boundary of a binary mask.
+def mask_to_boundary(mask, dilation_ratio=0.02):
+    """
+    Convert binary mask to boundary mask.
+    :param mask (numpy array, uint8): binary mask
+    :param dilation_ratio (float): ratio to calculate dilation = dilation_ratio * image_diagonal
+    :return: boundary mask (numpy array)
+    """
+    h, w = mask.shape
+    img_diag = np.sqrt(h ** 2 + w ** 2)
+    dilation = int(round(dilation_ratio * img_diag))
+    if dilation < 1:
+        dilation = 1
+    # Pad image so mask truncated by the image border is also considered as boundary.
+    new_mask = cv2.copyMakeBorder(mask, 1, 1, 1, 1, cv2.BORDER_CONSTANT, value=0)
+    kernel = np.ones((3, 3), dtype=np.uint8)
+    new_mask_erode = cv2.erode(new_mask, kernel, iterations=dilation)
+    mask_erode = new_mask_erode[1 : h + 1, 1 : w + 1]
+    # G_d intersects G in the paper.
+    return mask - mask_erode
+
+
+def boundary_iou(gt, dt, dilation_ratio=0.02):
+    """
+    Compute boundary iou between two binary masks.
+    :param gt (numpy array, uint8): binary mask
+    :param dt (numpy array, uint8): binary mask
+    :param dilation_ratio (float): ratio to calculate dilation = dilation_ratio * image_diagonal
+    :return: boundary iou (float)
+    """
+    gt_boundary = mask_to_boundary(gt, dilation_ratio)
+    dt_boundary = mask_to_boundary(dt, dilation_ratio)
+    intersection = ((gt_boundary * dt_boundary) > 0).sum()
+    union = ((gt_boundary + dt_boundary) > 0).sum()
+    boundary_iou = intersection / union
+    return boundary_iou
+
+
+if __name__ == "__main__":
+    ...
